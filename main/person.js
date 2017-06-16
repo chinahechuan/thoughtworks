d801fc01f9416dc0132e0df0ffa9c98bebf6faf7
chinahechuan <chinahechuan@hotmail.com>
2017-06-16 14:52:52 +0800

    thoughtworks summer vacation exam 04

--- main/person.js
+++ main/person.js
@@ -1,2 +1,10 @@
 // Write your code here
-module.exports = Person
+module.exports = class Person{
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+  introduce(){
+    return "My name is Tom. I am 21 years old.";
+  }
+}
--- main/student.js
+++ main/student.js
@@ -1,3 +1,11 @@
 let Person = require('./person')
 // Write your code here
-module.exports = Student
+module.exports = class Student extends Person{
+  constructor(name, age , kclass){
+    super(name, age);
+    this.kclass = kclass;
+  }
+  introduce(){
+    return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
+  }
+}
