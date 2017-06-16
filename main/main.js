376e59b71d70f622077ac52a556e8b5f5d0bf301
chinahechuan <chinahechuan@hotmail.com>
2017-06-16 10:18:59 +0800

    summer vacation exam 03

--- main/main.js
+++ main/main.js
@@ -1,3 +1,8 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your cade here
+  let sum = 0;
+  for(let index = 0; index < str.length; index++){
+    sum += (str.charAt(index)-'0');
+  }
+  return sum;
 };
